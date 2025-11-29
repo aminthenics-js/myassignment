815537667c4b88a09690a2ab14ea442e41449554
aminthenics-js <ar2069413@gmail.com>
2025-11-29 19:58:30 +0600

    my first assignment

---
+++ 01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 32409
+var accoutnEmail = "eidjf@google.com"
+let accountPassword = "4395-fhfdr84"
+accountCity = "Sylhet"
+let accountState;
+
+console.log(accountId);
+console.table([accoutnEmail, accountPassword, accountCity, accountState]);
+
+// Avoid using var
+
+var name = "Aminur";
+var name = "Rahman";
+
+console.log(name);
+console.log(name);
+
+{
+    let x = 10;
+    var y = 20;
+}
+console.log(y);
+//console.log(x);
